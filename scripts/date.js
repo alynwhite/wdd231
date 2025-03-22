018f4616aae74d4124bfbb437cdeee4c2aa3a9b1
alynwhite <alexia.white135@gmail.com>
2025-03-22 08:31:25 -0600

    added things

---
+++ scripts/date.js
@@ -0,0 +1,6 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+const today = new Date();
+
+currentyear.innerHTML = `<span>${today.getFullYear()}  | Alexia White | Utah, USA</span>  `;
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
